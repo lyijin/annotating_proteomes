c93b20bdddab15005617e290564c8fbb8cb0aefe
Yi Jin Liew <yijin.liew@kaust.edu.sa>
2018-01-14 18:35:05 +0300

    debugged a shell script

--- ftps_get_nr.sh
+++ ftps_get_nr.sh
@@ -3,5 +3,5 @@
 for a in {00..78}
 do
     echo "Grabbing nr.${a}.tar.gz"
-    ssh liewy@ftps.kaust.edu.sa "wget -q -O - ftp://ftp.ncbi.nlm.nih.gov/blast/db/nr.${a}.tar.gz" >> nr.${a}.tar.gz
+    wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/nr.${a}.tar.gz
 done
